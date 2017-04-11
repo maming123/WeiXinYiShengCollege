87acbd1d8b7d7fd0b63286b68ae99ef6a22b8818
maming123 <hurricanefbi@163.com>
2017-04-11 17:07:55 +0800

    初次添加

---
+++ WeiXinYiShengCollege/WeiXinYiShengCollege.Business/LogHelper.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using log4net;
+[assembly: log4net.Config.XmlConfigurator(Watch = true)]
+namespace WeiXinYiShengCollege.Business
+{
+    public class LogHelper
+    {
+        /// <summary>
+        /// 输出日志到Log4Net
+        /// </summary>
+        /// <param name="t"></param>
+        /// <param name="e"></param>
+        #region  static void WriteLog(Type t,Exception ex)
+        public static void WriteLogError(Type t, Exception e)
+        {
+            log4net.ILog log = log4net.LogManager.GetLogger(t);
+            log.Error("Error", e);
+        }
+        #endregion
+
+        /// <summary>
+        /// 输出日志到Log4Net
+        /// </summary>
+        /// <param name="t"></param>
+        /// <param name="msg"></param>
+        #region   static void WriteLog(Type t,string msg)
+        public static void WriteLogError(Type t, string msg)
+        {
+            log4net.ILog log = log4net.LogManager.GetLogger(t);
+            log.Error(msg);
+        }
+        /// <summary>
+        /// 输出日志到Log4Net
+        /// </summary>
+        /// <param name="t"></param>
+        /// <param name="msg"></param>
+       
+        public static void WriteLogInfo(Type t, string msg)
+        {
+            log4net.ILog log = log4net.LogManager.GetLogger(t);
+            log.Info(msg);
+        }
+
+        public static void WriteLogDebug(Type t, string msg)
+        {
+            log4net.ILog log = log4net.LogManager.GetLogger(t);
+            log.Debug(msg);
+        }
+        #endregion
+    }
+}
---
+++ WeiXinYiShengCollege/WeiXinYiShengCollege/Global.asax.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.SessionState;
+using log4net.Config;
+
+namespace WeiXinYiShengCollege
+{
+    public class Global : System.Web.HttpApplication
+    {
+
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            //应用程序启动时，自动加载配置log4Net  
+            XmlConfigurator.Configure();  
+        }
+
+        protected void Session_Start(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_AuthenticateRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Session_End(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_End(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
